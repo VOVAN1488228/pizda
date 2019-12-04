8e7fdcec0fb4d43936175b764848f8d7d01eabdc
429273-14 <429273-14>
2019-12-04 13:18:15 +0200

    second dick

--- index.php
+++ index.php
@@ -1,15 +1,72 @@
-п»їClass A{
+class VovanchesException extends RuntimeException {
 }
-Class B  extends A{
-	public a;
-	public b;
-	public c;
-	public d;
-	function__construct ($a, $b, $c, $d) {
-		$this->a = $a;
-		$this->b = $b;
-		$this->c = $c;
-		$this->d = $d;
+class A {
+	protected a;
+	protected b;
+	protected x;
+	function __construct($a, $b){
+		$this->a=$a;
+		$this->b=$b;
+	}
+	function ur($a, $b){
+		if ($a != 0) {
+			$x = -1*$b/$a;
+			$this->x = $x;
+			return $x;
+		}
+		throw new VovanchesException ("нет решения");	
 	}
 }
-$a=new B (new A(), new A(), new A(), new A());	
+class B extends A{
+	protected c;
+	protected x2;
+	function __construct($a, $b, $c){
+		parent::__construct($a, $b);
+		$this->c=$c;
+	}
+	protected function dir(($a, $b, $c) {
+		$dir = $b*$b - 4*$a*$c;
+		return $dir;
+	}
+	function ur2(($a, $b, $c) {
+		$dir = $this->dir();
+		if($a == 0){
+		   return $this->ur($a,$b);
+		   }
+		if ($dir > 0) {
+			$x = (-1*$b + sqrt($dir))/(2*$a);
+			$x2 = (-1*$b - sqrt($dir))/(2*$a);
+			$this->x = $x;
+			$this->x2 = $x2;
+			return array($x, $x2);
+		} else ($dir = 0) {
+			$x = (-1*$b)/(2*$a);
+			$this->x = $x;
+			return array($x);
+		}
+		throw new BuhalovaException ("нет решения");
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
