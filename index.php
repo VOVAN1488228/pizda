b6b153f4340bbad73266e34bd8a65878bf017c0a
429273-14 <429273-14>
2019-12-04 13:14:50 +0200

    first dick

---
+++ index.php
@@ -0,0 +1,15 @@
+﻿Class A{
+}
+Class B  extends A{
+	public a;
+	public b;
+	public c;
+	public d;
+	function__construct ($a, $b, $c, $d) {
+		$this->a = $a;
+		$this->b = $b;
+		$this->c = $c;
+		$this->d = $d;
+	}
+}
+$a=new B (new A(), new A(), new A(), new A());	
